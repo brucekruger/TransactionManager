44a6fba31750a477249a66ff2b15ebec6368e02c
baranau_an <andrei_baranau1@epam.com>
2021-10-04 18:30:00 +0300

    Added project to repo.

---
+++ TransactionManager/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Transactions;
+
+namespace TransactionManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vrm = new VolatileRM("RM1");
+            var vrm2 = new VolatileRM("RM2");
+            Console.WriteLine($"Member Value 1:{vrm.MemberValue}");
+            Console.WriteLine($"Member Value 2:{vrm2.MemberValue}");
+
+            using (var tsc = new TransactionScope())
+            {
+                vrm.MemberValue = 3;
+                vrm2.MemberValue = 5;
+                tsc.Complete();
+            }
+
+            Console.WriteLine($"Member Value 1:{vrm.MemberValue}");
+            Console.WriteLine($"Member Value 2:{vrm2.MemberValue}");
+        }
+    }
+}
---
+++ TransactionManager/VolatileRM.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Transactions;
+
+namespace TransactionManager
+{
+    public class VolatileRM : IEnlistmentNotification
+    {
+        private string _whoAmI = string.Empty;
+        private int _memberValue = 0;
+        private int _oldMemberValue = 0;
+        private bool _forceRollBack = false;
+
+        public int MemberValue
+        {
+            get => this._memberValue;
+            set
+            {
+                Transaction tran = Transaction.Current;
+                if(tran != null)
+                {
+                    Console.WriteLine($"{this._whoAmI}: MemberValue setter - EnlistVolatile");
+                    tran.EnlistVolatile(this, EnlistmentOptions.None);
+                }
+                this._oldMemberValue = this._memberValue;
+                this._memberValue = value;
+            }
+        }
+
+        public VolatileRM(string whoAmI, bool forceRollBack = false)
+        {
+            this._whoAmI = whoAmI;
+            this._forceRollBack = forceRollBack;
+        }
+
+        #region IEnlistmentNotification Members
+
+        public void Commit(Enlistment enlistment)
+        {
+            Console.WriteLine($"{this._whoAmI}: {nameof(Commit)}");
+
+            // Clear out _oldMemberValue
+            this._oldMemberValue = 0;
+            enlistment.Done();
+        }
+
+        public void InDoubt(Enlistment enlistment)
+        {
+            Console.WriteLine($"{this._whoAmI}: {nameof(InDoubt)}");
+            enlistment.Done();
+        }
+
+        public void Prepare(PreparingEnlistment preparingEnlistment)
+        {
+            Console.WriteLine($"{this._whoAmI}: {nameof(Prepare)}");
+            if (!this._forceRollBack)
+            {
+                preparingEnlistment.Prepared();
+            }
+            else
+            {
+                preparingEnlistment.ForceRollback();
+            }
+        }
+
+        public void Rollback(Enlistment enlistment)
+        {
+            Console.WriteLine($"{this._whoAmI}: {nameof(Rollback)}");
+
+            // Restore previous state
+            this._memberValue = this._oldMemberValue;
+            this._oldMemberValue = 0;
+            enlistment.Done();
+        }
+
+        #endregion
+    }
+}
